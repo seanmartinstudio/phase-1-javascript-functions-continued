442174ad8eaaa6f16a898bf40d5b56b32eb66f94
Sean Martin <sean@seanmartinstudio.com>
2021-11-07 11:13:27 -0800

    commit tree 8af027af34fa1c25f14d46dc9140535e9ff5f09c

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
+console.log(saturdayFun())
 
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+console.log(mondayWork());
 
-let wrapAdjective = function(style="*") {
+// let wrapAdjective = function(style="*") {
+//   return function(adjective="special") {
+//     return `You are ${style}${adjective}${style}!`
+//   }
+// }
+
+
+ function wrapAdjective(style="*") {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
   }
 }
+console.log(wrapAdjective());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function wrapAdjective() {
+//   return function(adjective = "special") {
+//     return `You are ${adjective}`
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
